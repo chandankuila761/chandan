ab1aceacddccdd2f9ed66287a08b757b1abf48bb
Chandan Kuila <chandankuila761@gmail.com>
2020-08-13 21:40:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Developement By Chandan</title>
+</head>
+
+<body>
+  <h1>My First GIT and HEROKU app</h1>
+  <p>I am very glad after uploading that</p>
+  <?php phpinfo(); ?>
+</body>
+
+</html>
