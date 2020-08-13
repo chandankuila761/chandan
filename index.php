07a656ae581363f8e80d68d5edc5526b14c487f6
Chandan Kuila <chandankuila761@gmail.com>
2020-08-13 22:22:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php 
+    require('../vendor/autoload.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Developement By Chandan</title>
 </head>
-
 <body>
   <h1>My First GIT and HEROKU app</h1>
   <p>I am very glad after uploading that</p>
   <?php phpinfo(); ?>
 </body>
-
 </html>
